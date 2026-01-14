caaabf015a7a105df5b05bffa1abdd1b132431f1
massi1234 <dziri_massinissa@hotmail.fr>
2026-01-14 19:21:04 +0100

    D

--- app/src/main/java/com/example/scannerpdfocr/util/OcrUtil.kt
+++ app/src/main/java/com/example/scannerpdfocr/util/OcrUtil.kt
@@ -3,12 +3,19 @@ package com.example.scannerpdfocr.util
 import android.graphics.Bitmap
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
 import kotlinx.coroutines.tasks.await
 
 object OcrUtil {
+
     suspend fun recognizeText(bitmap: Bitmap): String {
         val image = InputImage.fromBitmap(bitmap, 0)
-        val recognizer = TextRecognition.getClient()
+
+        // Client ML Kit avec options explicites (obligatoire dans ta version)
+        val recognizer = TextRecognition.getClient(
+                TextRecognizerOptions.DEFAULT_OPTIONS
+        )
+
         val result = recognizer.process(image).await()
         return result.text
     }
